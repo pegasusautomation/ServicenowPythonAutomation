b2d760847e3477ca685b77e08690da8cb2d111c7
Sowndarya <sowndu2001@gmail.com>
2023-09-08 17:38:07 +0530

    New commit

--- getAllIncidents.py
+++ getAllIncidents.py
@@ -16,4 +16,4 @@ res = requests.get(url,auth = ("admin","yw^zW!SiUP26"),headers = headers, data =
 psrsedData = res.json()
 for json_inner_data in psrsedData['result']:
         print(json_inner_data['number'] + " Updated on Date: " + json_inner_data['sys_updated_on'])
-print("Total Number Of Incedents: " + str(len(psrsedData['result'])))  
+print("Total Number Of Incedents: " + str(len(psrsedData['result'])))
