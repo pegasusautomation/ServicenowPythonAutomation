0e5f6c8c9981ebdeb5bc988c6d441db65f41eaf0
Sowndarya <sowndu2001@gmail.com>
2023-09-09 11:07:04 +0530

    Triggermailto team python file moved out form TEstScript

--- triggerMailToTeam.py
+++ triggerMailToTeam.py
@@ -2,7 +2,7 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login('suchiputti1@gmail.com','9738036466')
+server.login('devservicenow73@gmail.com','uljmyygvkgdhlnmj')
 
 server.sendmail('suchiputti1@gmail.com', 'ga.rag9@gmail.com','Mail sent form python')
 
