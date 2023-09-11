9e4f298f4b7fb2e35cbd66ab8fd4f2935017f004
Sowndarya <sowndu2001@gmail.com>
2023-09-11 19:00:35 +0530

    Send mail will all incident list

--- getAllIncidents.py
+++ getAllIncidents.py
@@ -1,19 +1,29 @@
 import requests
 import json
+class getAllIncidents:
+ 
+ def getincidentapi():
+        listOfIncidents = ["IncidentList"]
+        url = "https://dev179896.service-now.com/api/now/table/incident"
 
-url = "https://dev179896.service-now.com/api/now/table/incident"
+        headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        }
 
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
+        payload = {
+        "Short_Description":"Short desc"
+        }
+        res = requests.get(url,auth = ("admin","yw^zW!SiUP26"),headers = headers, data = json.dumps(payload))
 
-payload = {
-    "Short_Description":"Short desc"
-}
-
-res = requests.get(url,auth = ("admin","yw^zW!SiUP26"),headers = headers, data = json.dumps(payload))
-psrsedData = res.json()
-for json_inner_data in psrsedData['result']:
-        print(json_inner_data['number'] + " Updated on Date: " + json_inner_data['sys_updated_on'])
-print("Total Number Of Incedents: " + str(len(psrsedData['result'])))  
+        psrsedData = res.json()
+        for item in psrsedData['result']:
+            listOfIncidents.append(item['number'])
+        return listOfIncidents
+            # print(listOfIncident)
+# with open ('text.txt', 'w') as file:  
+#              for line_1 in listOfIncident:  
+#                  file.write(line_1)  
+#                  file.write('\n')
+            # print(listOfIncident['number'] + " Updated on Date: " + listOfIncident['sys_updated_on'])
+        # print("Total Number Of Incedents: " + str(len(psrsedData['result'])))
